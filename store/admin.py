0accc03774f38f5e46e59e912a29278ca985b208
Daniel Ofodile <ofodiledaniel20@gmail.com>
2022-12-28 17:25:01 +0100

    updates administration details

--- store/admin.py
+++ store/admin.py
@@ -19,4 +19,4 @@ admin.site.register(Customer)
 admin.site.register(Order)
 
 
-# username = Tanushree, email = tanushree7252@gmail.com, password = 1234
+# username = Daniel, email = ofodiledaniel20@gmail.com, password = 4321dcba
